aa53c7b1c6a8fc960850522c6b1678a4e7d56328
Abhishyant Khare <abhishyant@gmail.com>
2017-01-16 23:26:57 -0800

    made landing page responsive

--- codebase/.build/static/css/index.js
+++ codebase/.build/static/css/index.js
@@ -7,40 +7,50 @@ const {width, height, scale} = Dimensions.get("window"),
 
 export default StyleSheet.create({
     "mission": {
-        "backgroundColor": "rgba(0,0,0,0)",
+        "backgroundColor": "rgba(0, 0, 0, 0)",
         "color": "#2B4A89",
-        "marginTop": 190,
-        "marginLeft": 30 * vw
+        "marginTop": -70 * vh,
+        "position": "relative",
+        "marginLeft": 0
     },
     "mission-text": {
-        "fontFamily": "Avenir Light",
+        "textAlign": "center",
         "fontSize": 84
     },
     "bg": {
-        "backgroundColor": "rgba(0,0,0,0)",
+        "backgroundColor": "rgba(0, 0, 0, 0)",
         "color": "#2B4A89",
-        "marginTop": 10,
-        "marginLeft": 35.5 * vw
+        "marginTop": 10
     },
     "bg-text": {
         "fontFamily": "'MyriadPro'",
-        "fontSize": 28
+        "fontSize": 28,
+        "textAlign": "center"
     },
     "apply-container": {
         "marginTop": 175,
+        "marginLeft": 0,
         "width": 150
+    },
+    "topbgimg": {
+        "background": "url('../images/bg.png')",
+        "height": 100 * vh,
+        "zIndex": 10,
+        "marginTop": -5 * vh
     },
     "apply": {
         "border": "1.1px solid #2B4A89",
         "borderRadius": 7,
-        "paddingTop": 10,
-        "paddingBottom": 10,
-        "paddingLeft": 50,
-        "paddingRight": 50,
+        "paddingTop": 15,
+        "paddingBottom": 15,
+        "paddingLeft": 70,
+        "paddingRight": 70,
         "textAlign": "center",
         "color": "#2B4A89",
         "marginLeft": 45 * vw,
-        "textDecoration": "none"
+        "textDecoration": "none",
+        "fontFamily": "'MyriadPro'",
+        "fontSize": 20
     },
     "apply:hover": {
         "textDecoration": "none",
@@ -48,9 +58,10 @@ export default StyleSheet.create({
         "background": "#2B4A89"
     },
     "gear": {
-        "marginTop": 350,
-        "marginLeft": 46 * vw,
-        "width": 100
+        "marginTop": 200,
+        "marginLeft": 47 * vw,
+        "marginBottom": 10,
+        "width": 80
     },
     "projects-wrapper": {
         "marginTop": 3,
@@ -59,7 +70,8 @@ export default StyleSheet.create({
     "projects": {
         "fontSize": 30,
         "color": "#2B4A89",
-        "display": "inline"
+        "display": "inline",
+        "fontFamily": "'MyriadPro'"
     },
     "card": {
         "background": "#fff",
@@ -71,17 +83,18 @@ export default StyleSheet.create({
         "marginBottom": 1,
         "marginLeft": 1,
         "position": "relative",
-        "width": 325
+        "width": 325,
+        "fontFamily": "'MyriadPro'"
     },
     "card-1": {
         "marginTop": 30,
         "marginLeft": 10 * vw,
-        "boxShadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        "boxShadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
         "textAlign": "center"
     },
     "card-title": {
         "color": "#3C8721",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 50
     },
     "card-subtitle": {
@@ -95,72 +108,49 @@ export default StyleSheet.create({
         "fontSize": 16
     },
     "project-desc": {
-        "marginTop": 10,
+        "marginTop": 30,
         "marginRight": 30,
         "marginBottom": 10,
         "marginLeft": 30,
         "display": "inline-flex",
         "textAlign": "left",
-        "fontSize": 16
-    },
-    "circle1": {
-        "height": 100,
-        "width": 100,
-        "marginLeft": 110,
-        "border": "1px solid #3C8721",
-        "borderRadius": "50%",
-        "marginTop": 30
+        "fontSize": 15
     },
     "card-2": {
         "marginTop": 30,
         "marginLeft": 5 * vw,
-        "boxShadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        "boxShadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
         "textAlign": "center"
     },
     "card-title-2": {
         "color": "#D38742",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 50
     },
     "card-subtitle-2": {
         "color": "#566573",
         "fontSize": 18
     },
-    "circle2": {
-        "height": 100,
-        "width": 100,
-        "marginLeft": 110,
-        "border": "1px solid #D38742",
-        "borderRadius": "50%",
-        "marginTop": 30
-    },
     "card-3": {
         "marginTop": 30,
         "marginLeft": 5 * vw,
-        "boxShadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        "boxShadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
         "textAlign": "center"
     },
     "card-title-3": {
         "color": "#6D3BAD",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 50
     },
     "card-subtitle-3": {
         "color": "#566573",
         "fontSize": 18
     },
-    "circle3": {
-        "height": 100,
-        "width": 100,
-        "marginLeft": 110,
-        "border": "1px solid #6D3BAD",
-        "borderRadius": "50%",
-        "marginTop": 30
-    },
     "tie": {
-        "marginTop": 210,
-        "marginLeft": 46 * vw,
-        "width": 100
+        "marginTop": 100,
+        "marginLeft": 47 * vw,
+        "marginBottom": 10,
+        "width": 80
     },
     "card-comm": {
         "background": "#fff",
@@ -172,57 +162,79 @@ export default StyleSheet.create({
         "marginBottom": 1,
         "marginLeft": 1,
         "position": "relative",
-        "width": 295
+        "width": 285,
+        "fontFamily": "'MyriadPro'"
     },
     "card-comm-1": {
         "marginTop": 30,
-        "marginLeft": 5 * vw,
-        "boxShadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        "marginLeft": 6.25 * vw,
+        "boxShadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
         "textAlign": "center"
     },
     "comm-desc": {
-        "marginTop": 50,
+        "marginTop": 30,
+        "marginRight": 30,
+        "marginBottom": 30,
+        "marginLeft": 30,
         "display": "inline-flex",
-        "marginRight": 50,
-        "fontSize": 16
+        "textAlign": "left",
+        "fontSize": 15
     },
     "comm-list": {
         "listStyle": "none"
     },
-    "comm-img": {
-        "height": 100,
+    "comm-img-1": {
+        "height": 50,
         "width": "auto",
-        "marginTop": 100
+        "marginTop": 60
+    },
+    "comm-img-2": {
+        "height": 50,
+        "width": "auto",
+        "marginTop": 58
+    },
+    "comm-img-3": {
+        "height": 50,
+        "width": "auto",
+        "marginTop": 65
+    },
+    "comm-img-4": {
+        "height": 50,
+        "width": "auto",
+        "marginTop": 65
     },
     "card-comm-2": {
         "marginTop": 30,
         "marginLeft": 2 * vw,
-        "boxShadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        "boxShadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
         "textAlign": "center"
     },
     "card-title-4-1": {
         "color": "#436CD1",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 50
     },
     "card-title-4": {
         "color": "#436CD1",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 0
     },
     "comm-desc-4": {
         "marginTop": 15,
-        "marginRight": 50,
-        "fontSize": 16
+        "marginRight": 30,
+        "marginBottom": 30,
+        "marginLeft": 30,
+        "textAlign": "left",
+        "fontSize": 15
     },
     "card-title-5-1": {
         "color": "#6D3BAD",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 50
     },
     "card-title-5": {
         "color": "#6D3BAD",
-        "fontSize": 26,
+        "fontSize": 22,
         "marginTop": 0
     }
 });
