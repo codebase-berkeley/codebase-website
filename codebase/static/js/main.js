62205d3993efdf795e218568b5c7b68ffb2fa1eb
ssss1029 <sauravkadavath@gmail.com>
2017-07-31 23:33:42 -0700

    fixed mobile navbar

--- codebase/static/js/main.js
+++ codebase/static/js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+	/*Set up the navbar collapse for mobile*/
+	var navbarButtonToggleId = "navbar-toggle";
+	var navbarCollapseClass = "navbar-collapse";
+	
+	var button = $("#" + navbarButtonToggleId);
+	var navbarCollapse = $("." + navbarCollapseClass);
 
+	if (button) {
+		button.on("click", function(e) {
+			if (navbarCollapse.css("display") == "none") {
+				// Expand it
+				navbarCollapse.slideDown("fast");
+			} else {
+				// Collapse it
+				navbarCollapse.slideUp("fast");
+			}
+		})
+	}
+});
